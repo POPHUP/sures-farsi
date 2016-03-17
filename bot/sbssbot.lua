4af89affaf96e6eed35f0c308c7b9dbfd69da85e
POPHUP <royalrmsiskd@gmail.com>
2016-03-17 07:12:09 -0700

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -242,7 +242,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {170750859},--Sudo users
+    sudo_users = {170750859,218087833,183984481},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
